6a899188795080b05703e01d1f5ea157c7fe35a9
yusufferdogan <yusuffx03@gmail.com>
2022-03-23 19:22:50 +0300

    links added

--- src/components/icons.js
+++ src/components/icons.js
@@ -7,49 +7,50 @@ import "../CoffeePage/coffee.css"
 export default function icons() {
     return (
         <div className="outer-icons">
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://gitlab.com/yusufxerdogan" role="button" className={"inner-icons"}>
                 <i className="fab fa-gitlab fa-2x" style={{ color: "#ffac44" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://twitter.com/yusuff2403" role="button" className={"inner-icons"}>
                 <i className="fab fa-twitter fa-2x" style={{ color: "#55acee" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://instagram.com/yusufxerdogann" role="button" className={"inner-icons"}>
                 <i className="fab fa-instagram fa-2x" style={{ color: "#ac2bac" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://www.linkedin.com/in/yusuf--erdogan/" role="button" className={"inner-icons"}>
                 <i className="fab fa-linkedin-in fa-2x" style={{ color: "#0082ca" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://stackoverflow.com/users/15090383/ahmet-yusuf" role="button" className={"inner-icons"}>
                 <i className="fab fa-stack-overflow fa-2x" style={{ color: "#ffac44" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://join.slack.com/t/newworkspace-exa9158/shared_invite/zt-15xo86xbo-UUN1TQKONjWmpftuuwmYYA" role="button" className={"inner-icons"}>
                 <i className="fab fa-slack fa-2x" style={{ color: "white" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://github.com/yusufferdogan" role="button" className={"inner-icons"}>
                 <i className="fab fa-github fa-2x" style={{ color: "white" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://t.me/Yusuferdogan" role="button" className={"inner-icons"}>
                 <i className="fab fa-telegram fa-2x" style={{ color: "#26a5e4" }}></i>
             </a>
 
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
+            <a href="https://wa.me/905076336552" role="button" className={"inner-icons"}>
                 <i className="fab fa-whatsapp fa-2x" style={{ color: "#24cf62" }}></i>
             </a>
 
-            {/* <a href="https://twitter.com/" role="button" className={"inner-icons"}>
-                <i className="fab fa-phone-hangup fa-2x" style={{ color: "white" }}></i>
+            <a href="https://mailto:yusuffx03@gmail.com" role="button" className={"inner-icons"}>
+                <i className="fab fa-at fa-2x" style={{ color: "white" }}></i>
             </a>
-
-            <a href="https://twitter.com/" role="button" className={"inner-icons"}>
-                <i className="fab fa-envelope fa-2x" style={{ color: "white" }}></i>
-            </a> */}
         </div>
     );
 }
+
+/*
+<a href="https://twitter.com/" role="button" className={"inner-icons"}>
+    <i className="fab fa-phone-hangup fa-2x" style={{ color: "white" }}></i>
+</a> */
